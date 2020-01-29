2fe679a22d447bdf1d4efcc340efe93436b6ea5f
medeo <fasiellon@yahoo.it>
2020-01-29 10:35:11 +0100

    media mensile entry: handled comma insertion

--- src/window.js
+++ src/window.js
@@ -149,10 +149,14 @@ var NaspiCalculatorWindow = GObject.registerClass ({
                 return;
         }
         if (key_val == Gdk.keyval_from_name ('comma') ) {
-            // only one comma accepted
-            if (entry.get_text ().split (',').length-1 == 0) {
+            if (entry.get_text ().indexOf (',') != - 1) {
+				// there's alrealdy a comma
+				GObject.signal_stop_emission_by_name(entry, "key-press-event");
                 return;
-            }
+            } else {
+				this._onMoneyEntryCommaAdded (entry);
+				return;
+			}
         }
         if (key_val == Gdk.keyval_from_name ('BackSpace') ) {
             let cursor_pos = entry.get_position ();
@@ -331,5 +335,30 @@ var NaspiCalculatorWindow = GObject.registerClass ({
         GLib.idle_add (200, update_cursor_position);
     }
 
-    _onMoneyEntryCommaAdded () {}
+    _onMoneyEntryCommaAdded (entry) {
+
+		 function update_cursor_position () {
+            if (new_value.length > value.length) {
+                entry.set_position (cursor_pos + 1);
+            } else {
+                entry.set_position (cursor_pos);
+            }
+        }
+
+		let value = entry.get_text ();
+		let cursor_pos = entry.get_position ();
+		if (cursor_pos == value.length) {
+			// usual entry behaviour is ok, nothing to do
+			return;
+		}
+		let floor = value.slice (0, cursor_pos).replace (/\./g, '');
+		let decimal = value.slice (cursor_pos).replace (/\./g, '');
+		let dotted_floor = Util.add_dots (floor);
+		let new_value = dotted_floor + "," + decimal;
+		entry.set_text (new_value);
+
+		GObject.signal_stop_emission_by_name (entry, "key-press-event");
+		// move cursor accordingly
+        GLib.idle_add (200, update_cursor_position);
+	}
 });
